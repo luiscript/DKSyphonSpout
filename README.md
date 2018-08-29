9b2368b5655eed94f6ad0fd12439dbbd412c0900
luiscript <isc.luisfer@gmail.com>
2018-08-29 11:29:00 -0500

    ofxDarkKnightSyphon rises - support for syphon server

---
+++ README.md
@@ -0,0 +1,11 @@
+## ofxDarkKnightSyphon ##
+
+**Syphon server & client for ofxDarkKnight**
+
+## Dependencies
+- [ofxSyphon](https://github.com/astellato/ofxSyphon)
+
+## Tested on
+- Mac OSX 10.13.6  
+- oF 0.9.8  
+- Xcode 9.4.1
---
+++ src/ofxDarkKnightSyphon.cpp
@@ -0,0 +1,81 @@
+/*
+ Copyright (C) 2018 Luis Fernando García [http://luiscript.com]
+ 
+ Permission is hereby granted, free of charge, to any person obtaining a copy
+ of this software and associated documentation files (the "Software"), to deal
+ in the Software without restriction, including without limitation the rights
+ to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
+ 
+ The above copyright notice and this permission notice shall be included in all
+ copies or substantial portions of the Software.
+ 
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ SOFTWARE.
+ */
+
+#include "ofxDarkKnightSyphon.hpp"
+
+
+void ofxDarkKnightSyphon::setup()
+{
+    fbo = nullptr;
+    serverName = "output";
+}
+
+void ofxDarkKnightSyphon::update()
+{
+    
+}
+
+void ofxDarkKnightSyphon::draw()
+{
+    if(drawFbo)
+    {
+        syphonOut.publishTexture(&fbo->getTexture());
+    }
+    
+}
+
+void ofxDarkKnightSyphon::setFbo(ofFbo * fboPtr)
+{
+    fbo  = fboPtr;
+    drawFbo = fboPtr != nullptr;
+    if(drawFbo)
+    {
+        syphonOut.setName(serverName);
+    }
+}
+
+ofFbo * ofxDarkKnightSyphon::getFbo()
+{
+    return fbo;
+}
+
+
+void ofxDarkKnightSyphon::drawMasterOutput()
+{
+    drawOutputConnection();
+}
+
+void ofxDarkKnightSyphon::drawMasterInput()
+{
+    drawInputConnection();
+}
+
+void ofxDarkKnightSyphon::addModuleParameters()
+{
+    gui->onTextInputEvent(this, &ofxDarkKnightSyphon::onTextInputEvent);
+    gui->addTextInput("Server name", "output");
+}
+
+void ofxDarkKnightSyphon::onTextInputEvent(ofxDatGuiTextInputEvent e)
+{
+    serverName = e.target->getText();
+}
